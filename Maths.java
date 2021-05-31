acd56b3e1b10804cc68e03936b0cd01808e5056d
AnjaliPerera <kumudu079@gmal.com>
2021-05-31 18:42:57 +0530

    Write an equation

---
+++ Maths.java
@@ -0,0 +1,16 @@
+
+/**
+ * This class return the answer of an eqaution given the inputs.
+ */
+public class Maths {
+
+    public static void main(String[] args) {
+        double x, y, z, Answer;
+        x = 567;
+        y = 30;
+        z = 25;
+
+       Answer = x/y-z;
+        System.out.println("answer " + Answer);
+    }
+}
